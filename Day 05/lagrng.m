b6f78ed549e07e7238c310019df599ae37807e04
danshika-2003 <sivadanshi@gmail.com>
2026-03-28 19:04:56 +0530

    Add files via upload

---
+++ Day 05/lagrng.m
@@ -0,0 +1,16 @@
+function[sum]=lagrng(x,y,x0)
+
+n=length(x);
+v=zeros(n,1);
+
+sum=0;
+for i=1:n
+    product=y(i);
+    for j=1:n
+        if i~=j
+            product=product*(x0-x(j))/(x(i)-x(j));
+        end
+    end
+    sum=sum+product;
+end
+end
